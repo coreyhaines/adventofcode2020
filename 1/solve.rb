d0121f2616b0ef3a1790aebabbbed31a652a92e7
Corey Haines <coreyhaines@gmail.com>
2020-12-01 08:14:57 -0600

    Add some documentation for the first day's solution

--- 1/solve.rb
+++ 1/solve.rb
@@ -1,9 +1,13 @@
+# https://adventofcode.com/2020/day/1
+# Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+# Find three numbers in your expense report that meet the same criteria.
+
 f = File.read "./input.txt"
 total = 2020
 size = 3
 
-p f.lines
-  .map(&:to_i)
-  .combination(size)
-  .select{|group| group.sum == total}
-  .map{|group| group.reduce(&:*)}
+p f.lines # Read the numbers from the file
+  .map(&:to_i) # Convert them to integers
+  .combination(size) # Get all combinations of :size: groups
+  .select{|group| group.sum == total} # Find the group that sums to the :total:
+  .map{|group| group.reduce(&:*)} # Find the product of the group
